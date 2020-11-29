6a5e63b605b32dd55c9bdd49f41ad59cec520747
yanh1993 <891367701@qq.com>
2020-11-29 11:40:51 +0800

    'up'

---
+++ src/Core/Redis/RedisOptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Redis
+{
+    public class RedisOptions
+    {
+        /// <summary>
+        /// 连接
+        /// </summary>
+        public string Connection { get; set; }
+        /// <summary>
+        /// 数据库名称
+        /// </summary>
+        public string InstanceName { get; set; }
+        /// <summary>
+        /// 大小
+        /// </summary>
+        public string DefaultDB { get; set; }
+    }
+}
